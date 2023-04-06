7544fc0ebd7131bd5be4988887ab5a6540e4f9de
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-06 14:18:25 +0200

    types of operators

---
+++ operators_in_C/types_of_operators_in_C.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//operators in c
+//precedence of arithmetic operators
+
+int main()
+{
+    /*
+    //1 - arithmetic operators
+        {+, -, *, /, %}
+
+    //2- increment / decrement operators
+        {++, --}
+
+    //3- relational operators
+        {==, !=, <=, >=, <, >}
+
+    //4- logical operators
+        {&&, ||, !}
+
+    //5- bitwise operators
+        {&, ^, |, ~, >>, <<}
+
+    //6- assignment operators
+        {=, +=, -=< *=, /=, %=, <<=, >>=, &=, ^=, !=}
+
+    //7- other operators
+        {?:, &, *, sizeof(), ','}
+    */
+
+    int x, y;
+    scanf("%d %d", &x, &y);
+}
