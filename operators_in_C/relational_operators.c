eb8256fdc72b2fc42ba66f485dce476a9c003620
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-07 02:59:28 +0200

    relational operators

---
+++ operators_in_C/relational_operators.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//operators in c
+//relational operators
+
+int main()
+{
+    /*
+    //3- relational operators
+        {==, !=, <=, >=, <, >}
+
+    ► used for comparing two values
+    ► all relational operators will return either true or false
+
+    */
+
+    //example
+    int a = 300, b = 2090;
+
+    if (a >= b)
+    {
+        printf("Bingo! You are in");
+    }
+    else
+    {
+        printf("OOPS! Your are out");
+    }
+
+}
+
