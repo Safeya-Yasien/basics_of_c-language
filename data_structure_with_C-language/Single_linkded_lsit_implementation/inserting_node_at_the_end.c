90b23992090b01168b2f1838119a0341ce4496e4
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-10 03:10:43 +0200

    inserting node at the end of the single linked list

---
+++ data_structure_with_C-language/Single_linkded_lsit_implementation/inserting_node_at_the_end.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//inserting a node at the end of single linked list
+
+
+struct Node
+{
+    int data;
+    struct Node *link;
+};
+
+void add_at_end(struct Node *head, int data);
+void print_list(struct Node *head);
+
+int main()
+{
+    struct Node *head = NULL, *current = NULL;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = 50;
+    head->link = NULL;
+
+    current = (struct Node *)malloc(sizeof(struct Node));
+    current->data = 100;
+    current->link = NULL;
+    head->link = current;
+
+    current = (struct Node *)malloc(sizeof(struct Node));
+    current->data = 200;
+    current->link = NULL;
+    head->link->link = current;
+
+    add_at_end(head, 67);
+    print_list(head);
+
+}
+
+void add_at_end(struct Node *head, int data)
+{
+    struct Node *ptr, *temp;
+    ptr = head;
+
+    temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->link = NULL;
+
+    while (ptr->link != NULL)
+    {
+        ptr = ptr->link;
+    }
+    ptr->link = temp;
+
+}
+
+
+void print_list(struct Node *head)
+{
+    struct Node *ptr;
+    ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+}
