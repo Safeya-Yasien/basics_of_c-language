b61d61970264e21b5110d6427bb51808ac8e8dc6
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-12 06:57:02 +0200

    insert node at particular position

---
+++ data_structure_with_C-language/Single_linkded_lsit_implementation/insert_node_at_position.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    insert node at particular position
+*/
+
+struct Node
+{
+    int data;
+    struct Node *link;
+};
+
+void add_node_at_end(struct Node* head, int data);
+void add_at_pos(struct Node *head, int data, int pos);
+int main()
+{
+    struct Node *head = NULL, *ptr = NULL;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 50;
+    head->link = NULL;
+
+    add_node_at_end(head, 90);
+    add_node_at_end(head, 3);
+
+    int data = 67, position = 3;
+
+    add_at_pos(head, data, position);
+
+    ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+
+}
+
+void add_node_at_end(struct Node* head, int data)
+{
+    struct Node* new_node = NULL, *ptr = NULL;
+    ptr = head;
+    new_node = (struct Node *)malloc(sizeof(struct Node));
+
+    new_node->data = data;
+    new_node->link = NULL;
+
+    while (ptr->link != NULL)
+    {
+        ptr = ptr->link;
+    }
+
+    ptr->link = new_node;
+
+}
+
+void add_at_pos(struct Node *head, int data, int pos)
+{
+    struct Node* new_node = NULL, *ptr = NULL;
+    ptr = head;
+    new_node = (struct Node *)malloc(sizeof(struct Node));
+
+    new_node->data = data;
+    new_node->link = NULL;
+
+    pos--;
+
+    while (pos != 1)
+    {
+        ptr = ptr->link;
+        pos--;
+    }
+
+    new_node->link = ptr->link;
+    ptr->link = new_node;
+}
+
+
