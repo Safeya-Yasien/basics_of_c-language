1bc500704795479be157f42030597183a1cd8084
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-23 09:33:59 +0200

    add node to empty, beginning and end of the list

---
+++ data_structure_with_C-language/Double_linked_list/add_to_empty_beginning_end.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    add node to empty, beginning and end of the list
+*/
+
+struct Node
+{
+        int data;
+        struct Node *pre;
+        struct Node *next;
+};
+
+struct Node *addToEmpty(struct Node* head, int data);
+struct Node* addToBeg(struct Node *head, int data);
+struct Node* addToEnd(struct Node *head, int data);
+
+
+
+int main()
+{
+        struct Node *head = NULL, *ptr = NULL;
+        head = addToEmpty(head, 10);
+        head = addToBeg(head, 9);
+        head = addToEnd(head, 1);
+
+
+        ptr = head;
+
+        while (ptr != NULL)
+        {
+            printf("%d ", ptr->data);
+            ptr = ptr->next;
+        }
+
+}
+
+struct Node* addToEnd(struct Node *head, int data)
+{
+        struct Node *new_node = NULL, *ptr = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+
+        new_node->data = data;
+        new_node->pre = NULL;
+        new_node->next = NULL;
+
+        ptr = head;
+
+        while (ptr->next != NULL)
+            ptr = ptr->next;
+
+        ptr->next = new_node;
+        new_node->pre = ptr;
+
+        return (head);
+}
+
+struct Node* addToBeg(struct Node *head, int data)
+{
+        struct Node *new_node = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+
+        new_node->data = data;
+        new_node->pre = NULL;
+        new_node->next = NULL;
+
+        head->pre = new_node;
+        new_node->next = head;
+
+        head = new_node;
+
+        return (head);
+}
+
+struct Node *addToEmpty(struct Node* head, int data)
+{
+        struct Node *new_node = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+
+        new_node->data = data;
+        new_node->pre = NULL;
+        new_node->next = NULL;
+
+        head = new_node;
+
+        return (head);
+}
+
