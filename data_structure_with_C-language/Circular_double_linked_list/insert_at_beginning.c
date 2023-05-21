b04a5c2517ee88448b963ed63fd99989db8dd257
Safeya-Yasien <bluesky7yas@gmail.com>
2023-05-21 23:37:29 +0300

    insert at the beginning of the circular doubly linked list

---
+++ data_structure_with_C-language/Circular_double_linked_list/insert_at_beginning.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+        insert node at the beginning of circular doubly linked list
+*/
+
+struct Node
+{
+        int data;
+        struct Node* prev;
+        struct Node *next;
+};
+
+struct Node* addToEmpty(int data);
+struct Node* addToBeg(struct Node* tail, int data);
+void display(struct Node* tail);
+
+
+
+int main()
+{
+        struct Node* tail = NULL;
+        tail = addToEmpty(20);
+        tail = addToBeg(tail, 10);
+
+        display(tail);
+}
+
+struct Node* addToEmpty(int data)
+{
+        struct Node* new_node = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+        new_node->prev = new_node;
+        new_node->data = data;
+        new_node->next = new_node;
+
+        return (new_node);
+}
+
+struct Node* addToBeg(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->prev = tail;
+        new_node->next = temp;
+        temp->prev = new_node;
+        tail->next = new_node;
+
+        return (tail);
+
+}
+
+void display(struct Node* tail)
+{
+        struct Node* temp = tail->next;
+
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != tail->next);
+}
+
