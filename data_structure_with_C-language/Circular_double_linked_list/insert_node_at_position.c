d0a095aeb5eefb04bba513f9045b84c8bd54bf87
Safeya-Yasien <bluesky7yas@gmail.com>
2023-05-22 10:13:14 +0300

    insert node after particular position

---
+++ data_structure_with_C-language/Circular_double_linked_list/insert_node_at_position.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+        insert node after particular position
+*/
+
+struct Node
+{
+        int data;
+        struct Node* prev;
+        struct Node *next;
+};
+
+struct Node* addToEmpty(int data);
+struct Node* addToBeg(struct Node* tail, int data);
+struct Node* addToEnd(struct Node* tail, int data);
+struct Node* addAfterPos(struct Node* tail, int pos, int data);
+void display(struct Node* tail);
+
+
+int main()
+{
+        struct Node* tail;
+        tail = addToEmpty(20);
+        tail = addToBeg(tail, 10);
+        tail = addToEnd(tail, 30);
+        tail = addToEnd(tail, 40);
+        tail = addAfterPos(tail, 3, 70);
+
+        display(tail);
+}
+
+struct Node* addToEmpty(int data)
+{
+        struct Node* new_node = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+        new_node->prev = new_node;
+        new_node->data = data;
+        new_node->next = new_node;
+
+        return (new_node);
+}
+
+struct Node* addToBeg(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->next = temp;
+        new_node->prev = tail;
+        tail->next = new_node;
+        temp->prev = new_node;
+
+        return (tail);
+
+}
+
+struct Node* addToEnd(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->prev = tail;
+        new_node->next = temp;
+        tail->next = new_node;
+        temp->prev = new_node;
+
+        tail = new_node;
+
+        return (tail);
+}
+
+struct Node* addAfterPos(struct Node* tail, int pos, int data)
+{
+        struct Node* new_node, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+        while (pos > 1)
+        {
+            temp = temp->next;
+            pos--;
+        }
+
+        new_node->prev = temp;
+        new_node->next = temp->next;
+        temp->next->prev = new_node;
+        temp->next = new_node;
+
+        if (temp == tail)
+            tail = tail->next;
+
+        return (tail);
+}
+
+void display(struct Node* tail)
+{
+        struct Node* temp = tail->next;
+
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != tail->next);
+}
+
+
