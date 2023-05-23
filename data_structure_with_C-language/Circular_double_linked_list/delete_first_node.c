9a76eef42ba81dd127e04def8e5682b8e0d927c0
Safeya-Yasien <bluesky7yas@gmail.com>
2023-05-23 22:00:47 +0300

    delete first node

---
+++ data_structure_with_C-language/Circular_double_linked_list/delete_first_node.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+        delete first node
+*/
+
+struct Node
+{
+        int data;
+        struct Node* prev;
+        struct Node *next;
+};
+
+struct Node* addToEmpty(int data);
+struct Node* addToBeg(struct Node* tail, int data);
+struct Node* addToEnd(struct Node* tail, int data);
+struct Node* delAtBeg(struct Node* tail);
+void display(struct Node* tail);
+
+
+int main()
+{
+        struct Node* tail;
+        tail = addToEmpty(20);
+        tail = addToBeg(tail, 10);
+        tail = addToEnd(tail, 30);
+        tail = addToEnd(tail, 40);
+        tail = delAtBeg(tail);
+
+        display(tail);
+}
+
+struct Node* addToEmpty(int data)
+{
+        struct Node* new_node = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+        new_node->prev = new_node;
+        new_node->data = data;
+        new_node->next = new_node;
+
+        return (new_node);
+}
+
+struct Node* addToBeg(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->next = temp;
+        new_node->prev = tail;
+        tail->next = new_node;
+        temp->prev = new_node;
+
+        return (tail);
+
+}
+
+struct Node* addToEnd(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->prev = tail;
+        new_node->next = temp;
+        tail->next = new_node;
+        temp->prev = new_node;
+
+        tail = new_node;
+
+        return (tail);
+}
+
+struct Node* delAtBeg(struct Node* tail)
+{
+        if (tail == NULL)
+            return (tail);
+
+        struct Node* temp = tail->next, *current = NULL;
+
+        if (temp == tail)
+        {
+            free(tail);
+            tail = NULL;
+            return (tail);
+        }
+        current = temp->next;
+
+        current->prev = tail;
+        tail->next = current;
+
+        free(temp);
+
+        return (tail);
+
+}
+
+
+void display(struct Node* tail)
+{
+        struct Node* temp = tail->next;
+
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != tail->next);
+}
+
+
