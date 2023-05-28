7c492ea80c8a1aa163329310da7b0218dcdc57d0
Safeya-Yasien <bluesky7yas@gmail.com>
2023-05-28 14:52:38 +0300

    delete node at particular position

---
+++ data_structure_with_C-language/Circular_double_linked_list/delete_node_at_position.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+        delete node at particular position
+*/
+
+struct Node
+{
+        int data;
+        struct Node* prev;
+        struct Node *next;
+};
+
+struct Node* addToEmpty(int data);
+struct Node* addToBeg(struct Node* tail, int data);
+struct Node* addToEnd(struct Node* tail, int data);
+struct Node* delAtBeg(struct Node* tail);
+struct Node* delAtEnd(struct Node* tail);
+struct Node* delAtPos(struct Node* tail, int pos);
+void display(struct Node* tail);
+
+
+int main()
+{
+        struct Node* tail;
+        tail = addToEmpty(20);
+        tail = addToBeg(tail, 10);
+        tail = addToEnd(tail, 30);
+        tail = addToEnd(tail, 40);
+        //tail = delAtBeg(tail);
+        //tail = delAtBeg(tail);
+        //tail = delAtEnd(tail);
+        tail = delAtPos(tail, 3);
+
+        display(tail);
+}
+
+struct Node* addToEmpty(int data)
+{
+        struct Node* new_node = NULL;
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+        new_node->prev = new_node;
+        new_node->data = data;
+        new_node->next = new_node;
+
+        return (new_node);
+}
+
+struct Node* addToBeg(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->next = temp;
+        new_node->prev = tail;
+        tail->next = new_node;
+        temp->prev = new_node;
+
+        return (tail);
+
+}
+
+struct Node* addToEnd(struct Node* tail, int data)
+{
+        struct Node* new_node = NULL, *temp = NULL;
+        new_node = addToEmpty(data);
+
+        if (tail == NULL)
+            return (new_node);
+
+        temp = tail->next;
+
+        new_node->prev = tail;
+        new_node->next = temp;
+        tail->next = new_node;
+        temp->prev = new_node;
+
+        tail = new_node;
+
+        return (tail);
+}
+
+struct Node* delAtBeg(struct Node* tail)
+{
+        if (tail == NULL)
+            return (tail);
+
+        struct Node* temp = tail->next, *current = NULL;
+
+        if (temp == tail)
+        {
+            free(tail);
+            tail = NULL;
+            return (tail);
+        }
+        current = temp->next;
+
+        current->prev = tail;
+        tail->next = current;
+
+        free(temp);
+
+        return (tail);
+
+}
+
+struct Node* delAtEnd(struct Node* tail)
+{
+        if (tail == NULL)
+            return (tail);
+
+        struct Node* temp = tail->prev;
+
+        if (temp == tail)
+        {
+            free(tail);
+            tail = NULL;
+            return (tail);
+        }
+
+        temp->next = tail->next;
+        tail->next->prev = temp;
+
+        free(tail);
+        tail = temp;
+
+        return (tail);
+
+}
+
+struct Node* delAtPos(struct Node* tail, int pos)
+{
+        struct Node* temp = tail->next;
+        while (pos > 1)
+        {
+            temp = temp->next;
+            pos--;
+        }
+
+        struct Node* ptr = temp->prev;
+        ptr->next = temp->next;
+        temp->next->prev = ptr;
+        free(temp);
+
+        if (temp == tail)
+            tail = ptr;
+
+        return (tail);
+
+}
+
+void display(struct Node* tail)
+{
+        struct Node* temp = tail->next;
+
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != tail->next);
+}
+
+
